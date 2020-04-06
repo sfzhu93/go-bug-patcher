eb2b9bc74dee0fbf04d9b5a6e6fc288b6716e15b
Shuofei Zhu <suz305@lrs-song01.ist.psu.edu>
2020-04-05 22:30:52 -0400

    a rename

---
+++ scripts/overhead_docker_integration.py
@@ -0,0 +1,29 @@
+import sys
+
+
+def parseInput(file, testcasename):
+    durations = []
+    with open(file) as f:
+        for line in f:
+            if testcasename in line:
+                if 'PASS' in line:
+                    _, tmp = line.split('(')
+                    tmp = tmp.split('s')[0]
+                    durations.append(float(tmp))
+    return durations
+
+
+if __name__ == '__main__':
+    raw = sys.argv[1]
+    change = sys.argv[2]
+    num = int(sys.argv[3])
+    testcasename = sys.argv[4]
+
+    rawList = parseInput(raw, testcasename)
+    changeList = parseInput(change, testcasename)
+
+    assert (len(rawList) % num == 0)
+    assert (len(changeList) % num == 0)
+    assert (len(rawList) == len(changeList))
+
+    print('overhead:', (sum(changeList) - sum(rawList)) / sum(rawList))
